d1933f12fd666efb868e592ab14298b2fa9ac2d9
Soudip <soudip.bluehorse@gmail.con>
2026-01-10 12:38:31 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_map_app/pages/map_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:MapPage(),
+    );
+  }
+}
